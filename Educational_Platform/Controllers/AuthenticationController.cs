211899904088edf2a6aeb340d2f4346a52aa3e06
Hussein Awad <husseinhamed845@gmail.com>
2024-09-11 15:26:40 +0300

    Add Cors

--- Educational_Platform/Controllers/AuthenticationController.cs
+++ Educational_Platform/Controllers/AuthenticationController.cs
@@ -50,6 +50,44 @@ namespace Educational_Platform.Controllers
         }
 
         [HttpGet]
+        [Route("signupTeacher/{userID}/{academicDegree}/{experience}/{phoneNumber}/{birthdate}")]
+        public string SignUpTeacher(string userID, string academicDegree, string experience, string phoneNumber, string birthdate)
+        {
+            Teacher teacher = new Teacher();
+            teacher.UserID = int.Parse(userID);
+            teacher.AcademicDegree = academicDegree;
+            teacher.Experience = experience;
+            teacher.PhoneNumber = int.Parse(phoneNumber);
+            teacher.Birthdate = birthdate;
+            _DatabaseContext.Teacher.Add(teacher);
+            _DatabaseContext.SaveChanges();
+
+            JavaScriptSerializer JsData = new JavaScriptSerializer();
+            JsData.MaxJsonLength = int.MaxValue;
+            string Result = JsData.Serialize(teacher);
+            return Result;
+        }
+
+        [HttpGet]
+        [Route("signupStudent/{userID}/{classStudent}/{academicDegree}/{interests}")]
+        public string SignUpStudent(string userID, string classStudent, string academicDegree, string interests)
+        {
+            Student student = new Student();
+            student.UserID = int.Parse(userID);
+            student.Class = classStudent;
+            student.AcademicDegree = academicDegree ;
+            student.Interests = interests;
+
+            _DatabaseContext.Student.Add(student);
+            _DatabaseContext.SaveChanges();
+
+            JavaScriptSerializer JsData = new JavaScriptSerializer();
+            JsData.MaxJsonLength = int.MaxValue;
+            string Result = JsData.Serialize(student);
+            return Result;
+        }
+
+        [HttpGet]
         [Route("login/{email}/{password}")]
         public string LoginUser(string email, string password)
         {
--- Educational_Platform/Program.cs
+++ Educational_Platform/Program.cs
@@ -6,11 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ServerConnection")));
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+                      builder => builder.AllowAnyOrigin()
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AllowAllOrigins");
 
 // Configure the HTTP request pipeline.
 
